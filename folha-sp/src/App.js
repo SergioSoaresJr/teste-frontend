f4027590eed04083bc12e56a30643406e47fc926
Sérgio L. Soares Jr <sergio.l.soares@outlook.com>
2019-11-23 18:06:19 -0300

    Adicionado Highlights

--- folha-sp/src/App.js
+++ folha-sp/src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 import './styles.css'
 import Header from './components/Header';
+import Top from './components/TopNoticias';
 
 function App(){
   return(
     <div className="App">
+      <body>
         <Header />
+
+        <main>
+          <Top />
+        </main>
+
+      </body>
   </div>
   );
 }
---
+++ folha-sp/src/components/TopNoticias/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Highlight01 from '../../images/desktop/highlight-01.png'
+import Highlight02 from '../../images/desktop/highlight-02.png'
+import Highlight03 from '../../images/desktop/highlight-03.png'
+
+import './styles.css';
+
+function Top() {
+    return (
+        <div className="top-naticias"> 
+
+            <h2 className="highlights-title">Top cacildis</h2>
+           
+            <figure className="highlights">
+                <img className="highlights-img" src={Highlight01} alt="girl"/>
+                <figcaption className="highlights-caption"><span>pindureta</span></figcaption>
+            </figure>
+
+            <figure className="highlights">
+                <img className="highlights-img" src={Highlight02} alt="rhino"/>
+                <figcaption className="highlights-caption"><span>bolis</span></figcaption>
+            </figure>
+
+            <figure className="highlights">
+                <img className="highlights-img" src={Highlight03} alt="bird"/>
+                <figcaption className="highlights-caption"><span>sapien</span></figcaption>
+            </figure>
+        </div>
+    );
+} 
+
+export default Top;
